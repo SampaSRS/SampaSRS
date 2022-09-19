6bc793041a1474bc1a3019f857ff9a26431059ca
geovaneg <geovane.grossi@hotmail.com>
2022-09-19 15:27:59 -0300

    reading sampa and channels -> we need to change their type of variable!!

--- read_tree.cxx
+++ read_tree.cxx
@@ -5,6 +5,8 @@
 
 #include "TFile.h"
 #include "TTreeReader.h"
+#include "TTreeReaderArray.h"
+
 
 void read_tree(std::string_view file_name = "Run47.root")
 {
@@ -15,13 +17,16 @@ void read_tree(std::string_view file_name = "Run47.root")
   TFile file(file_name.data(), "READ");
   TTreeReader reader("waveform", &file);
   TTreeReaderValue<std::vector<std::vector<short>>> words(reader, "words"); // template type must match datatype
+  TTreeReaderArray<unsigned char> sampa(reader, "sampa");
+  TTreeReaderArray<unsigned char> channel(reader, "channel");
 
 int event_id = 0;
-  while (reader.Next() && event_id < 10) { //getting only 10 evts
+  while (reader.Next() && event_id < 10) {
     auto& event_words = *words;
     long sum = 0;
-    for (size_t i = 0; i < event_words.size(); ++i) { //acessing waveform i
-      for (size_t j = 0; j < event_words[i].size(); ++j) { //acessing word j of waveform i
+    for (size_t i = 0; i < event_words.size(); ++i) {
+      std::cout << (int)channel[i] <<" "<<(int)sampa[i]<<std::endl;
+      for (size_t j = 0; j < event_words[i].size(); ++j) {
         sum += event_words[i][j];
       }
     }
