3688849b87a22ab5b2a6ddf74ab74c8bf89377a0
geovanegrossi <souzageo@cvut.cz>
2022-10-12 19:25:48 -0300

    create pedestal file ready

--- create_pedestal.cpp
+++ create_pedestal.cpp
@@ -1,5 +1,6 @@
 
 #include <cmath>
+#include <math.h>
 #include <sampasrs/root_fix.hpp>
 
 #include <sampasrs/acquisition.hpp>
@@ -24,7 +25,7 @@ int main(int argc, const char* argv[])
   using namespace sampasrs;
 
   if (argc < 2) {
-    std::cerr << "Usage: create_pedestal <Pedestal_file.raw>\n";
+    std::cerr << "Usage: create_pedestal <Pedestal_file.root>\n";
     return 1;
   }
   const char* input_name = argv[1];
@@ -47,6 +48,7 @@ int main(int argc, const char* argv[])
 
   std::vector<double>sum(128);
   std::vector<double>count(128);
+  std::vector<double>sumsq(128);
   std::vector<double>mean(128);
   std::vector<double>Var(128);
   std::vector<double>Chn(128);
@@ -55,53 +57,22 @@ int main(int argc, const char* argv[])
 
   int const NumEvts = 10; //Number of events for pedestal file
 
-
   int event_id = 0;
-  while (reader.Next() || event_id<NumEvts) 
-  {
-    auto& event_words = *words;
-    for (size_t i = 2; i < event_words.size(); ++i) 
-    {
-      gl_chn = (sampa[i]-8)*32+channel[i];
-      // std::cout<<gl_chn<<std::endl;
-      for (size_t j = 0; j < event_words[i].size(); ++j) 
-      {
-        sum[gl_chn] += event_words[i][j];
-        count[gl_chn]++;
-        Chn[gl_chn]=channel[i];
-        Sampa[gl_chn]=sampa[i];
-      }
-    }
-    event_id++;
-  }
-
-  for(int k = 0; k<128; k++)
-  {
-    if(count[k] == 0)
-    {
-      mean[k]=-1;
-    }
-    else
-    {
-      mean[k] = sum[k]/count[k];
-    }
-  }
-
-
-
-  event_id=0;
-  reader.SetEntry(0);
   while (reader.Next() || event_id<NumEvts) 
   {
     auto& event_words = *words;
     for (size_t i = 0; i < event_words.size(); ++i) 
     {
       gl_chn = (sampa[i]-8)*32+channel[i];
+      Chn[gl_chn]=channel[i];
+      Sampa[gl_chn]=sampa[i];
       // std::cout<<gl_chn<<std::endl;
-      for (size_t j = 2; j < event_words[i].size(); ++j)  //partir do 2 para eliminar o 0 e numero de palavras do inicio
+      for (size_t j = 2; j < event_words[i].size(); ++j) 
       {
-        Var[gl_chn] += pow((event_words[i][j] - mean[gl_chn]),2);
-        // std::cout <<event_words[i][j] <<" "<< mean[gl_chn]<<std::endl;
+        sum[gl_chn] += event_words[i][j];
+        sumsq[gl_chn] += pow(event_words[i][j],2);
+        count[gl_chn]++;
+        // if(sampa[i]==9&&channel[i]==16)std::cout<<event_words[i][j]<<std::endl;
       }
     }
     event_id++;
@@ -111,15 +82,23 @@ int main(int argc, const char* argv[])
   {
     if(count[k] == 0)
     {
-    std::cout <<Sampa[k]<<" "<<Chn[k]<<" "<< -1 <<" "<<-1<<std::endl; //colocar qual valor? negativo não manda nada  
+    // TxtOutFile <<Sampa[k]<<" "<<Chn[k]<<" "<< -1 <<" "<<-1<<std::endl; //colocar qual valor? negativo não manda nada  
     }
     else
     {
     mean[k] = sum[k]/count[k];
-    std::cout <<Sampa[k]<<" "<<Chn[k]<<" "<<mean[k]<<" "<<std::sqrt(Var[k]/count[k])<<std::endl;  
+    Var[k] = sumsq[k]/count[k]-pow(sum[k]/count[k],2);
+    if(Var[k]==0)
+    {
+      // TxtOutFile <<Sampa[k]<<" "<<Chn[k]<<" "<<mean[k]<<" "<<-1<<std::endl;  
+    }
+    else
+    {
+      // TxtOutFile <<Sampa[k]<<" "<<Chn[k]<<" "<<mean[k]<<" "<<sqrt(Var[k])<<std::endl;
+      TxtOutFile <<k<<" "<<mean[k]<<" "<<sqrt(Var[k])<<std::endl;  
+    }
     }
   }
-
 
   TxtOutFile.close();
   return 0;
