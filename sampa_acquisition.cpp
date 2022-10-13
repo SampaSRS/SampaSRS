e9bd263ca0189759a2c00d08a1d86688d095d14b
Thiago Badaró <thiago.saramela@usp.br>
2022-10-13 17:29:44 -0300

    Fix: prevent sampa_acquisition from using the resource of an entire CPU

--- sampa_acquisition.cpp
+++ sampa_acquisition.cpp
@@ -1,10 +1,10 @@
 #include <sampasrs/acquisition.hpp>
-#include <sampasrs/utils.hpp>
 
 #include <fmt/format.h>
 
 #include <chrono>
 #include <string>
+#include <thread>
 
 int main(int argc, const char* argv[])
 {
@@ -21,14 +21,11 @@ int main(int argc, const char* argv[])
   const bool save_raw = true;
   sampasrs::Acquisition sampa(file_prefix, save_raw, {}, address); // Start aquisition
 
-  sampasrs::Timer info_timer(std::chrono::milliseconds(1000));
-
   // Loop forever
   while (true) {
-    if (info_timer) {
-      const auto stats = sampa.get_stats();
-      fmt::print("Events recorded: {} | Events/s {:5.2f} | Invalid events {:5.2f} % | Buffer usage: {:5.2f} % | Net speed: {:5.2f} MB/s | Write speed {:5.2f} MB/s\n",
-          stats.valid_events, stats.valid_event_rate, stats.invalid_event_ratio, stats.write_buffer_use, stats.read_speed, stats.write_speed);
-    }
+    std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+    const auto stats = sampa.get_stats();
+    fmt::print("Events recorded: {} | Events/s {:5.2f} | Invalid events {:5.2f} % | Buffer usage: {:5.2f} % | Net speed: {:5.2f} MB/s | Write speed {:5.2f} MB/s\n",
+        stats.valid_events, stats.valid_event_rate, stats.invalid_event_ratio, stats.write_buffer_use, stats.read_speed, stats.write_speed);
   }
 }
