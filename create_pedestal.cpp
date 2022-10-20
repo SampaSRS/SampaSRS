e0e2e85bd9b832be684c5a81fe65789894bdb0da
Thiago Badaró <thiago.saramela@usp.br>
2022-10-20 15:53:11 -0300

    Remove hard coded channel range from create_pedestal

--- create_pedestal.cpp
+++ create_pedestal.cpp
@@ -1,15 +1,11 @@
-
-#include <cmath>
-#include <math.h>
 #include <sampasrs/root_fix.hpp>
 
+#include <cmath>
 #include <cstddef>
 #include <filesystem>
 #include <fstream>
-#include <iostream>
-#include <numeric>
+#include <map>
 #include <string>
-#include <string_view>
 #include <vector>
 
 #include <TFile.h>
@@ -25,12 +21,7 @@ int main(int argc, const char* argv[])
   const char* input_name = argv[1];
 
   auto input_path = std::filesystem::path(input_name);
-  bool read_raw = input_path.extension().string() == ".root";
   auto rootfname = input_path.replace_extension("PedFile.txt").string();
-
-  size_t n_events = 0;
-  std::cout << "Generating pedestal file: " << rootfname << "\n";
-  std::ofstream TxtOutFile(rootfname);
 
   TFile file(input_name, "READ");
   TTreeReader reader("waveform", &file);
@@ -38,48 +29,49 @@ int main(int argc, const char* argv[])
   TTreeReaderArray<short> sampa(reader, "sampa");
   TTreeReaderArray<short> channel(reader, "channel");
 
-  std::vector<double> sum(128);
-  std::vector<double> count(128);
-  std::vector<double> sumsq(128);
-  std::vector<double> mean(128);
-  std::vector<double> Var(128);
-  std::vector<double> Chn(128);
-  std::vector<double> Sampa(128);
-  int gl_chn = 0;
+  struct Pedestal {
+    short sampa = -1;
+    short channel = -1;
+    size_t count = 0;
+    double sum = 0;
+    double sum_squared = 0;
+  };
+
+  std::map<int, Pedestal> channels {};
 
   int const NumEvts = 10; // Number of events for pedestal file
 
   int event_id = 0;
-  while (reader.Next() || event_id < NumEvts) {
+  while (reader.Next() && event_id < NumEvts) {
     auto& event_words = *words;
     for (size_t i = 0; i < event_words.size(); ++i) {
-      gl_chn = (sampa[i] - 8) * 32 + channel[i];
-      Chn[gl_chn] = channel[i];
-      Sampa[gl_chn] = sampa[i];
-      // std::cout<<gl_chn<<std::endl;
+      const int global_channel = (sampa[i] - 8) * 32 + channel[i];
+      auto& pedestal = channels[global_channel];
+
+      // Fill info on first occurrence
+      if (pedestal.count == 0) {
+        pedestal.sampa = sampa[i];
+        pedestal.channel = channel[i];
+      }
+
       for (size_t j = 2; j < event_words[i].size(); ++j) {
-        sum[gl_chn] += event_words[i][j];
-        sumsq[gl_chn] += pow(event_words[i][j], 2);
-        count[gl_chn]++;
-        // if(sampa[i]==9&&channel[i]==16)std::cout<<event_words[i][j]<<std::endl;
+        pedestal.sum += event_words[i][j];
+        pedestal.sum_squared += std::pow(event_words[i][j], 2);
+        ++pedestal.count;
       }
     }
     event_id++;
   }
 
-  for (int k = 0; k < 128; k++) {
-    if (count[k] == 0) {
-      // TxtOutFile <<Sampa[k]<<" "<<Chn[k]<<" "<< -1 <<" "<<-1<<std::endl; //colocar qual valor? negativo não manda nada
-    } else {
-      mean[k] = sum[k] / count[k];
-      Var[k] = sumsq[k] / count[k] - pow(sum[k] / count[k], 2);
-      if (Var[k] == 0) {
-        // TxtOutFile <<Sampa[k]<<" "<<Chn[k]<<" "<<mean[k]<<" "<<-1<<std::endl;
-      } else {
-        // TxtOutFile <<Sampa[k]<<" "<<Chn[k]<<" "<<mean[k]<<" "<<sqrt(Var[k])<<std::endl;
-        TxtOutFile << k << " " << mean[k] << " " << sqrt(Var[k]) << std::endl;
-      }
-    }
+  std::cout << "Generating pedestal file: " << rootfname << "\n";
+  std::ofstream TxtOutFile(rootfname);
+
+  TxtOutFile << "sampa channel global_channel mean std\n";
+
+  for (auto& [global_channel, pedestal] : channels) {
+    const double mean = pedestal.sum / static_cast<double>(pedestal.count);
+    const double var = pedestal.sum_squared / static_cast<double>(pedestal.count) - std::pow(mean, 2);
+    TxtOutFile << pedestal.sampa << " " << pedestal.channel << " " << global_channel << " " << mean << " " << std::sqrt(var) << "\n";
   }
 
   TxtOutFile.close();
