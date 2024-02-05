a75dcaecf0791dbd5d840fdc0bd8e04d346c6e04
geovanegrossi <geovane.grossi@gmail.com>
2024-02-05 15:16:07 -0300

    map pedestals inside mapping file

--- include/sampasrs/mapping.hpp
+++ include/sampasrs/mapping.hpp
@@ -4,11 +4,6 @@
 #include <cstring> 
 #include <cstdio>
 #include <vector>
-
-namespace sampasrs {
-
-
-#include <iostream>
 #include <string>
  
 inline void Mapping_strips(std::unordered_map<int, std::pair<double, double>> &my_map)
@@ -34,4 +29,37 @@ inline void Mapping_strips(std::unordered_map<int, std::pair<double, double>> &m
     mapfile.close();
 }
 
+inline void Map_pedestal(std::string const& pedestal_file, std::unordered_map<int, std::pair<double, double>> &my_map)
+{
+    // Create an unordered_map of three values glchn(sampa*32+Chn)    
+    std::ifstream mapfile;
+    int glchn=0;
+    double baseline=0;
+    double sigma=0;
+    mapfile.open(pedestal_file);
+    while (true) 
+    { 
+      mapfile >> glchn;
+      mapfile >> baseline;
+      mapfile >> sigma;
+      if(sigma==0)
+      {
+        my_map[glchn] = {baseline,1023}; //supress the channels with sigma == 0 / are frozen
+      }
+      else
+      {
+        my_map[glchn] = {baseline,sigma};
+      }
+      // std::cout << glchn << " " << my_map[glchn].first <<" "<<my_map[glchn].second<<std::endl;
+      
+
+      if( mapfile.eof() ) 
+      {
+        break;
+      }
+    }
+    mapfile.close();
+  
+
 }
+
