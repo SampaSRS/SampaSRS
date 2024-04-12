75f5bd716c8e2dda1464fcf9298f8a05bc45d81c
geovanegrossi <geovane.grossi@gmail.com>
2024-04-12 16:15:33 -0300

    clustering working for both zs and non zs method

--- clustering.cpp
+++ clustering.cpp
@@ -18,138 +18,6 @@
 
 using namespace std;
 
-const int max_time_window = 10; //maximum time difference before the maximum to be checked at 20MSps, 1 = 50ns
-const int Min_Number_words = 5; //Minimum number of ADC samples to consider a cluster valid  
-
-struct Hits_evt {
-  int gl_chn;
-  std::vector <int> time;
-  std::vector <int> word;
-  double x_pos;
-};
-
-bool sort_by_chn(const Hits_evt& a, const Hits_evt& b)
-{
-    // smallest comes first
-    return a.x_pos < b.x_pos;
-}
-
-
-
-std::pair<int, int> GetMaxWord(std::vector <Hits_evt> hits)
-{
-
-  int max_word=0;
-  int max_channel=0;
-  int Last_word = 0;
-  for (int i = 0; i< hits.size(); i++)
-  {
-    for(int j = 0; j<hits[i].time.size(); j++)
-    {
-      if(hits[i].word[j]>Last_word)
-      {
-        max_word=j;
-        max_channel=i;
-        Last_word = hits[i].word[j];
-      }
-    }
-  }
-  return std::make_pair(max_channel, max_word);
-}
-
-void Make_Cluster(std::vector <Hits_evt> hits, std::vector <int> &ClustSize,std::vector <double> &ClustTime, 
-std::vector <double> &ClustPos,std::vector <double> &ClustEnergy)
-
-{  
-  double pitch = 0.390625; //pitch real = 0.390625
-  double xcm=0;
-  double E_total=0;
-  int ClstID=0;
-  int ClstSize=1;
-  int MaxNClust=0;
-  double ClstTime = 0;
-  std::vector <double> del_index = {};
-  std::vector <double> del_words = {};
-  int word_count = 0;
-  while(hits.size()>0)
-  {
-
-
-
-    auto max_idx = GetMaxWord(hits);
-    // std::cout <<"New max: "<<max_idx.first <<" "<<max_idx.second <<std::endl;
-
-      //     for(int i = 0; i<hits.size(); i++)
-      // {
-      //   std::cout <<"Index: "<< i <<" Position: "<<hits[i].x_pos <<std::endl;
-      //   for(int j = 0; j<hits[i].word.size(); j++){
-      //     if(i == max_idx.first && j == max_idx.second) std::cout <<"***T: "<<hits[i].time[j]<<" / Amp: "<< hits[i].word[j]<<" ***** ---- ";
-      //     else{
-      //     std::cout <<"T: "<<hits[i].time[j]<<" / Amp: "<< hits[i].word[j]<<" ---- ";
-      //     }
-      //   }
-      //   std::cout<<std::endl;
-      // }
-
-      for (int i = 0; i< hits.size(); i++)
-      {
-        for(int j = 0; j<hits[i].time.size(); j++)
-        {
-        if( abs(hits[max_idx.first].x_pos-hits[i].x_pos) <= 2*pitch )
-          {
-            if(abs( hits[max_idx.first].time[max_idx.second]-hits[i].time[j]) <= max_time_window/2)      //maximum time separation
-            {
-              xcm += hits[i].x_pos*hits[i].word[j];
-              E_total += hits[i].word[j];
-              ClstTime += hits[i].time[j]*hits[i].word[j];
-              del_words.push_back(j);
-              word_count++;
-
-
-            }
-
-          }
-        }
-
-        for (unsigned k = del_words.size(); k-- > 0; )
-          {
-            hits[i].word.erase(hits[i].word.begin()+del_words.at(k));
-            hits[i].time.erase(hits[i].time.begin()+del_words.at(k));        
-          }
-            del_words.clear();
-      }
-
-
-  for(int i = 0; i<hits.size(); i++)
-  {
-    if(hits[i].word.size()==0)
-    del_index.push_back(i);
-  } 
-
-  for (unsigned k = del_index.size(); k-- > 0; )
-  {
-    hits.erase(hits.begin()+del_index.at(k));  
-  }
-  del_index.clear();
-
-  if(word_count>=Min_Number_words)
-  {
-    ClustSize.push_back(ClstSize);
-    ClustPos.push_back(xcm/E_total);
-    ClustEnergy.push_back(E_total);
-    ClustTime.push_back(ClstTime/E_total);
-    // std::cout <<"The new clusters: "<<ClstSize<< " "<<xcm/E_total<<" "<<E_total<<" "<<ClstTime/E_total<<std::endl;
-    // std::cout <<"------------------------- NEXT -----------------" <<std::endl;
-  }
-
-  MaxNClust++;
-  xcm = 0;
-  E_total = 0;
-  ClstTime = 0;
-  ClstSize = 1;
-  word_count = 0;
-  }
-}
 
 
 int main(int argc, char *argv[])
@@ -234,6 +102,8 @@ int main(int argc, char *argv[])
   std::array<double, 1024> n_chns={};
   std::array<double, 1024> sum_cm={};
 
+  int Entries;
+  Entries = reader.GetEntries();
 
   std::vector <Hits_evt> hits; 
   bool evt_ok=false;
@@ -271,9 +141,9 @@ int main(int argc, char *argv[])
       gl_chn = 32*(sampa[i]-8)+channel[i];
       for (size_t j = 2; j < event_words[i].size(); ++j) 
       { 
-        if(event_words[i][j] > 0 && event_words[i][j]<1024)
+        if(event_words[i][j] >= 0 && event_words[i][j]<1024)
         {
-          if(event_words[i][j] > map_of_pedestals[gl_chn].first+3*map_of_pedestals[gl_chn].second+sum_cm[j]/n_chns[j])
+          if(event_words[i][j] > map_of_pedestals[gl_chn].first+4*map_of_pedestals[gl_chn].second+sum_cm[j]/n_chns[j])
           { 
             time_hit.push_back(j);  //The sampa structure is [Number of samples, Initial time, words ....] so K must be reduced by 1 
             word_hit.push_back(event_words[i][j]-map_of_pedestals[gl_chn].first-sum_cm[j]/n_chns[j]);
@@ -309,16 +179,16 @@ int main(int argc, char *argv[])
     }
 
     ++event_id;
-    if(event_id % 10000==0)
+    if(event_id % 50000==0)
     {
-      std::cout << event_id <<" events analyzed -- " <<num_bad_evt<<" bad events." <<std::endl;
+      std::cout << "Progress: "<<(double)(event_id*100/Entries)<<"% - "<< event_id <<" events analyzed -- " <<num_bad_evt<<" bad events." <<std::endl;
 
     }
 
     if(hits.size()>0)
     {
       std::sort(hits.begin(), hits.end(), sort_by_chn);
-      Make_Cluster(hits, CSize, ClstTime, ClstPosX, ClstEnergy);
+      Make_1D_Strip_Cluster(hits, CSize, ClstTime, ClstPosX, ClstEnergy);
     }
     hits.clear();
 
--- include/sampasrs/cluster.hpp
+++ include/sampasrs/cluster.hpp
@@ -3,9 +3,140 @@
 #include <vector>
 #include <iostream>
 
+struct Hits_evt {
+  int gl_chn;
+  std::vector <int> time;
+  std::vector <int> word;
+  double x_pos;
+};
+
+bool sort_by_chn(const Hits_evt& a, const Hits_evt& b)
+{
+    // smallest comes first
+    return a.x_pos < b.x_pos;
+}
 
 
-void Make_Cluster(std::vector<std::tuple<int, double, int, int >> hit,std::vector <int> &ClustSize,std::vector <double> &ClustTime, 
+
+std::pair<int, int> GetMaxWord(std::vector <Hits_evt> hits)
+{
+
+  int max_word=0;
+  int max_channel=0;
+  int Last_word = 0;
+  for (int i = 0; i< hits.size(); i++)
+  {
+    for(int j = 0; j<hits[i].time.size(); j++)
+    {
+      if(hits[i].word[j]>Last_word)
+      {
+        max_word=j;
+        max_channel=i;
+        Last_word = hits[i].word[j];
+      }
+    }
+  }
+  return std::make_pair(max_channel, max_word);
+}
+
+void Make_1D_Strip_Cluster(std::vector <Hits_evt> hits, std::vector <int> &ClustSize,std::vector <double> &ClustTime, 
+std::vector <double> &ClustPos,std::vector <double> &ClustEnergy)
+
+{  
+  double pitch = 0.390625; //pitch real = 0.390625
+  const int max_time_window = 10; //maximum time difference before the maximum to be checked at 20MSps, 1 = 50ns
+  const int Min_Number_words = 5; //Minimum number of ADC samples to consider a cluster valid  
+  double xcm=0;
+  double E_total=0;
+  int ClstID=0;
+  int ClstSize=1;
+  int MaxNClust=0;
+  double ClstTime = 0;
+  std::vector <double> del_index = {};
+  std::vector <double> del_words = {};
+  int word_count = 0;
+  while(hits.size()>0)
+  {
+
+
+
+    auto max_idx = GetMaxWord(hits);
+    // std::cout <<"New max: "<<max_idx.first <<" "<<max_idx.second <<std::endl;
+
+      //     for(int i = 0; i<hits.size(); i++)
+      // {
+      //   std::cout <<"Index: "<< i <<" Position: "<<hits[i].x_pos <<std::endl;
+      //   for(int j = 0; j<hits[i].word.size(); j++){
+      //     if(i == max_idx.first && j == max_idx.second) std::cout <<"***T: "<<hits[i].time[j]<<" / Amp: "<< hits[i].word[j]<<" ***** ---- ";
+      //     else{
+      //     std::cout <<"T: "<<hits[i].time[j]<<" / Amp: "<< hits[i].word[j]<<" ---- ";
+      //     }
+      //   }
+      //   std::cout<<std::endl;
+      // }
+
+      for (int i = 0; i< hits.size(); i++)
+      {
+        for(int j = 0; j<hits[i].time.size(); j++)
+        {
+        if( abs(hits[max_idx.first].x_pos-hits[i].x_pos) <= 2*pitch )
+          {
+            if(abs( hits[max_idx.first].time[max_idx.second]-hits[i].time[j]) <= max_time_window/2)      //maximum time separation
+            {
+              xcm += hits[i].x_pos*hits[i].word[j];
+              E_total += hits[i].word[j];
+              ClstTime += hits[i].time[j]*hits[i].word[j];
+              del_words.push_back(j);
+              word_count++;
+
+
+            }
+
+          }
+        }
+
+        for (unsigned k = del_words.size(); k-- > 0; )
+          {
+            hits[i].word.erase(hits[i].word.begin()+del_words.at(k));
+            hits[i].time.erase(hits[i].time.begin()+del_words.at(k));        
+          }
+            del_words.clear();
+      }
+
+
+  for(int i = 0; i<hits.size(); i++)
+  {
+    if(hits[i].word.size()==0)
+    del_index.push_back(i);
+  } 
+
+  for (unsigned k = del_index.size(); k-- > 0; )
+  {
+    hits.erase(hits.begin()+del_index.at(k));  
+  }
+  del_index.clear();
+
+  if(word_count>=Min_Number_words)
+  {
+    ClustSize.push_back(ClstSize);
+    ClustPos.push_back(xcm/E_total);
+    ClustEnergy.push_back(E_total);
+    ClustTime.push_back(ClstTime/E_total);
+    // std::cout <<"The new clusters: "<<ClstSize<< " "<<xcm/E_total<<" "<<E_total<<" "<<ClstTime/E_total<<std::endl;
+    // std::cout <<"------------------------- NEXT -----------------" <<std::endl;
+  }
+
+  MaxNClust++;
+  xcm = 0;
+  E_total = 0;
+  ClstTime = 0;
+  ClstSize = 1;
+  word_count = 0;
+  }
+}
+
+
+void Make_Cluster_Old(std::vector<std::tuple<int, double, int, int >> hit,std::vector <int> &ClustSize,std::vector <double> &ClustTime, 
 std::vector <double> &ClustPos,std::vector <double> &ClustEnergy)
 
 {  
